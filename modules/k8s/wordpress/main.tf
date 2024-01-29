f95444d7c8c76769650ce02c6fcd96657e46ad56
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2024-01-29 16:59:50 -0300

    Add Wordpress

---
+++ modules/k8s/wordpress/main.tf
@@ -0,0 +1,48 @@
+resource "juju_model" "model" {
+  name = var.juju_model_name
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+module "k8s_mysql" {
+  source = "github.com/terraform-modules.git//modules/k8s/mysql"
+
+  juju_model_name = juju_model.model.name
+  cloud_name      = local.cloud_name
+}
+
+resource "juju_application" "k8s_wordpress" {
+  name  = var.wordpress_application_name
+  model = juju_model.model.name
+
+  charm {
+    name    = "wordpress-k8s"
+    channel = var.wordpress_charm_channel
+  }
+
+  config = {
+    blog_hostname    = var.wordpress_application_name
+    initial_settings = <<EOF
+    user_name: admin
+    admin_email: devnull@example.com
+    EOF
+  }
+
+  units = 1
+}
+
+resource "juju_integration" "k8s_wordpress_k8s_mysql" {
+  model = juju_model.model.name
+
+  application {
+    name     = var.wordpress_application_name
+    endpoint = "database"
+  }
+
+  application {
+    name     = module.k8s_mysql.application_name
+    endpoint = "database"
+  }
+}
+
