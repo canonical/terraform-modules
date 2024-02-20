16f5351ea55038d613b236c915e4edebadaa345e
Amanda H. L. de Andrade Katz <amanda.katz@canonical.com>
2024-02-20 09:27:29 -0300

    Update main.tf

--- modules/k8s/synapse/main.tf
+++ modules/k8s/synapse/main.tf
@@ -6,7 +6,7 @@ resource "juju_model" "model" {
 }
 
 module "k8s_postgresql" {
-  source = "github.com/terraform-modules.git//modules/k8s/postgresql"
+  source = "github.com/canonical/terraform-modules.git//modules/k8s/postgresql"
 
   juju_model_name = juju_model.model.name
   cloud_name      = local.cloud_name
