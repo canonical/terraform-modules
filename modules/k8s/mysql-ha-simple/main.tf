f960b92da9cb2b915956f0d3277232ec7b0fadd8
Paulo Machado <paulo.machado@canonical.com>
2024-09-27 10:18:08 -0300

    simple ha module

---
+++ modules/k8s/mysql-ha-simple/main.tf
@@ -0,0 +1,21 @@
+resource "juju_application" "mysql" {
+  name  = "mysql"
+  model = var.juju_model_name
+  trust = true
+
+  charm {
+    name     = "mysql-k8s"
+    channel  = var.mysql_charm_channel
+    revision = var.mysql_charm_revision
+    base     = var.mysql_charm_base
+  }
+
+  config = var.mysql_charm_config
+
+  storage_directives = {
+    database = var.mysql_storage_size
+  }
+
+  units       = var.mysql_charm_units
+  constraints = var.mysql_charm_constraints
+}
---
+++ modules/k8s/mysql-ha-simple/variables.tf
@@ -0,0 +1,45 @@
+variable "juju_model_name" {
+  description = "Juju model name"
+  type        = string
+}
+
+variable "mysql_charm_channel" {
+  description = "MySQL K8s charm channel"
+  type        = string
+  default     = "8.0/stable"
+}
+
+variable "mysql_charm_revision" {
+  description = "MySQL K8s charm revision"
+  type        = number
+  default     = 180
+}
+
+variable "mysql_charm_base" {
+  description = "MySQL K8s charm base"
+  type        = string
+  default     = "ubuntu@22.04"
+}
+
+variable "mysql_charm_units" {
+  description = "MySQL K8s charm units number"
+  type        = number
+  default     = 3
+}
+
+variable "mysql_charm_constraints" {
+  type    = string
+  default = ""
+}
+
+variable "mysql_storage_size" {
+  description = "MySQL storage size"
+  type        = string
+  default     = "10G"
+}
+
+variable "mysql_charm_config" {
+  description = "MySQL charm configuration"
+  type        = map(string)
+  default     = {}
+}
