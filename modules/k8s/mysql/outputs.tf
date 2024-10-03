320f8b24186e909ada8670e428b07797c8509693
Paulo Machado <paulo.machado@canonical.com>
2024-10-03 16:33:10 -0300

    expose provides and requires for COS and database

--- modules/k8s/mysql/outputs.tf
+++ modules/k8s/mysql/outputs.tf
@@ -1,3 +1,18 @@
 output "application_name" {
   value = juju_application.k8s_mysql.name
 }
+
+
+output "provides" {
+  value = {
+    database          = "database",
+    metrics_endpoint  = "metrics-endpoint",
+    grafana_dashboard = "grafana-dashboard"
+  }
+}
+
+output "requires" {
+  value = {
+    logging = "logging"
+  }
+}
